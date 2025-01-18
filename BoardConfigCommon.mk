61679f8806b615bbdba0984ce3ada9272a7cfac9
Rve27 <rve27github@gmail.com>
2025-01-18 20:07:39 +0700

    sdm845-common: Adapt to YAAP
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,9 +41,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := vendor/xiaomi/mi845_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 
-# Platform
-TARGET_BOARD_PLATFORM := sdm845
-
 # Audio
 TARGET_PROVIDES_AUDIO_EXTNS := true
 
@@ -63,7 +60,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/yaap/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
--- sdm845.mk
+++ sdm845.mk
@@ -16,10 +16,13 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 
+# Platform
+TARGET_BOARD_PLATFORM := sdm845
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
